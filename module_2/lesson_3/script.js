63cd4ee497c6285b0bbd5f3ef3746ecf310e1546
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2025-08-12 22:14:40 +0300

    lesson_3

---
+++ module_2/lesson_3/script.js
@@ -0,0 +1,74 @@
+// -------------------------------------------- 3
+
+/**
+ * Напиши скрипт для відображення годин та хвилин у консолі браузера у вигляді
+ * рядка формату "14 г. 26 хв.". Якщо значення змінної minutes дорівнює 0,
+ * то виводь рядок "14 г.", без хвилин.
+ */
+
+
+// const hours = 14;
+// const minutes = 0;
+// let time;
+
+// if(minutes > 0) {
+//     time = `${hours} г. ${minutes} хв.`;
+// } else {
+//     time = `${hours} г.`
+// }
+
+// console.log(time);
+
+
+
+
+// -------------------------------------- 6
+
+/*
+ * Напиши скрипт перевірки підписки користувача при доступі до контента
+ * - Є три типа підписки: free, pro і vip.
+ * - Отримати доступ можуть тільки користувачі pro і vip
+ */
+
+
+// const sub = "pro";
+// let canConnect;
+
+// //        false || true
+// if(sub === "pro" || sub === "vip") {
+//     canConnect = true;
+// } else {
+//     canConnect = false;
+// }
+
+// console.log("canConnect", canConnect);
+
+
+
+
+
+/*
+ * Напиши скрипт, який перевіряє можливість відкрити чат з користувачем.
+ * Для цього, користувач має бути:
+ * - другом
+ * - онлайн
+ * - без режима не турбувати
+ */
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnD = true;
+
+// //                      true && true && false
+// const canOpenChat = isOnline && isFriend && !isDnD;
+
+// console.log("canOpenChat", canOpenChat);
+
+
+
+
+
+
+
+
+
